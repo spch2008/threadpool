c62938176c7e274fdb6474e063a5af3fae4d92d8
spch2008 <spch2008@foxmail.com>
2016-05-16 00:43:26 +0800

    ADD: add task command

---
+++ include/task.h
@@ -0,0 +1,17 @@
+/**
+* @file   task.h
+* @author sunpengcheng(spch2008@foxmail.com)
+* @date   2016-05-16 00:40:58
+* @brief
+**/
+
+#ifndef _TASK_H_
+#define _TASK_H_
+
+class Task
+{
+public:
+    virtual void Exec() = 0;
+};
+
+#endif
