e59cb76e4fdd8fa79adf2925168833cbed78ce5c
spch2008 <spch2008@foxmail.com>
2016-05-29 18:04:02 +0800

    ADD: add thread pool

---
+++ test/test_pool.cpp
@@ -0,0 +1,55 @@
+/**
+* @file   test_pool.cpp
+* @author sunpengcheng(spch2008@foxmail.com)
+* @date   2016-05-21 09:48:32
+* @brief
+**/
+
+#include "task.h"
+#include "thread_pool.h"
+
+#include <vector>
+#include <unistd.h>
+#include <iostream>
+using namespace std;
+
+class LogTask : public Task
+{
+public:
+    void Exec()
+    {
+        write(0, "Log Done!\n", 10);
+        sleep(1);
+    }
+};
+
+int main()
+{
+    size_t log_cnt = 100;
+
+    vector<Task*> log_tasks;
+    for (size_t i = 0; i < log_cnt; i++)
+    {
+        Task *task = new LogTask;
+        log_tasks.push_back(task);
+    }
+
+    ThreadPool thread_pool;
+    thread_pool.Init(10);
+
+    for (size_t i = 0; i < log_cnt; i++)
+    {
+        thread_pool.AddTask(log_tasks[i]);
+    }
+
+    thread_pool.Start();
+    thread_pool.WaitForAllDone();
+
+    for (size_t i = 0; i < log_cnt; i++)
+    {
+        delete log_tasks[i];
+    }
+    log_tasks.clear();
+
+    return 0;
+}
